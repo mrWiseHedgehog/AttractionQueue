ae2efc81c5451578514ed0a6dd4cc1168bf866da
Leonid <ithedgehog@yandex.ru>
2022-09-05 02:06:56 +0300

    Added src

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+
+public class Main {
+    public Main() {
+    }
+
+    public static List<Person> generateClients(List<Person> list) {
+        list.add(new Person("Kevin", "Mitnick", 3));
+        list.add(new Person("Adrian", "Lamo", 2));
+        list.add(new Person("Albert", "Gonzales", 5));
+        list.add(new Person("Jeanson", "James", 4));
+        list.add(new Person("Aaron", "Swartz", 7));
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Person> list = new LinkedList();
+        Deque<Person> clients = new ArrayDeque(generateClients(list));
+
+        while(!clients.isEmpty()) {
+            Person unit = (Person)clients.poll();
+            --unit.ticket;
+            System.out.println("The " + unit + " visited the attraction " + unit.ticket + " left");
+            if (unit.ticket != 0) {
+                clients.offer(unit);
+            }
+        }
+
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,45 @@
 public class Person {
+    protected String name;
+    protected String lastName;
+    protected int ticket;
+
+    public Person(String name, String lastname, int ticket) {
+        this.name = name;
+        this.lastName = lastname;
+        this.ticket = ticket;
+    }
+
+    public Person(String name) {
+        this.name = name;
+        this.lastName = this.lastName;
+        this.ticket = this.ticket;
+    }
+
+    public String toString() {
+        return this.name + " " + this.lastName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setName(int ticket) {
+        this.ticket = ticket;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLastname() {
+        return this.lastName;
+    }
+
+    public int getTicket() {
+        return this.ticket;
+    }
 }
